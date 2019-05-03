5914c356e50643b4cb141b71f96dd533733d0fa7
zoraiard <zoraiarodrigues@gmail.com>
2019-05-02 23:28:41 -0300

    Initial commit

---
+++ src/br/ufrn/imd/controllers/CadastroMBean.java
@@ -0,0 +1,39 @@
+package br.ufrn.imd.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.ufrn.imd.dominio.Pessoa;
+
+@ManagedBean 
+@SessionScoped 
+public class CadastroMBean {
+ 
+	//atributo pessoa
+	Pessoa pessoa;
+	
+	/** 
+		* Construtor 
+	*/ 
+	public CadastroMBean() { 
+		pessoa = new Pessoa(); 
+	} 
+	  
+	public Pessoa getPessoa() { 
+		return pessoa; 
+	} 
+	  
+	public void setPessoa(Pessoa pessoa) { 
+		this.pessoa = pessoa; 
+	} 
+	  
+	/** 
+		* Método para realização do cadastro. 
+		* @return 
+	*/ 
+
+	public String cadastrar() { 
+		return "/sucesso.jsf"; 
+	} 
+
+}
